28676722f2c49d7652bf86bd7734d0e7d49c12fb
yangjianbang <yangjianbang@baidu.com>
2025-12-30 23:47:30 +0800

    add github link

--- evalscope-jsonl-viewer/src/App.vue
+++ evalscope-jsonl-viewer/src/App.vue
@@ -5,28 +5,51 @@
 
 <template>
   <div>
-    <el-menu
-      mode="horizontal"
-      :default-active="$route.path"
-      class="nav-menu"
-      @select="handleSelect"
-      background-color="#f5f7fa"
-      text-color="#606266"
-      active-text-color="#409eff"
-    >
-      <el-menu-item index="/reviews" class="has-icon">
-        <el-icon style="margin-right: 6px; color: #409eff; font-size: 18px">
-          <Document />
-        </el-icon>
-        Reviews
-      </el-menu-item>
-      <el-menu-item index="/predictions" class="has-icon">
-        <el-icon style="margin-right: 6px; color: #409eff; font-size: 18px">
-          <DataAnalysis />
-        </el-icon>
-        Predictions
-      </el-menu-item>
-    </el-menu>
+    <div class="nav-wrapper">
+      <el-menu
+        mode="horizontal"
+        :default-active="$route.path"
+        class="nav-menu"
+        @select="handleSelect"
+        background-color="#f5f7fa"
+        text-color="#606266"
+        active-text-color="#409eff"
+      >
+        <el-menu-item index="/reviews" class="has-icon">
+          <el-icon style="margin-right: 6px; color: #409eff; font-size: 18px">
+            <Document />
+          </el-icon>
+          Reviews
+        </el-menu-item>
+        <el-menu-item index="/predictions" class="has-icon">
+          <el-icon style="margin-right: 6px; color: #409eff; font-size: 18px">
+            <DataAnalysis />
+          </el-icon>
+          Predictions
+        </el-menu-item>
+      </el-menu>
+
+      <a
+        class="github-link"
+        href="https://github.com/dynamicheart/llm-eval-viewer"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="GitHub Repository"
+      >
+        <svg
+          height="24"
+          width="24"
+          viewBox="0 0 16 16"
+          fill="#409eff"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2 .37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.6 7.6 0 012-.27c.68.01 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.28.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8 8 0 0016 8c0-4.42-3.58-8-8-8z"
+          />
+        </svg>
+      </a>
+    </div>
 
     <header class="page-header">
       <img
@@ -67,10 +90,36 @@ export default {
 </script>
 
 <style scoped>
+.nav-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #f5f7fa;
+  padding: 0 16px;
+  box-shadow: 0 2px 4px rgb(0 0 0 / 0.1);
+}
+
 .nav-menu {
   font-weight: 600;
   font-size: 16px;
-  box-shadow: 0 2px 4px rgb(0 0 0 / 0.1);
+  flex: 1 1 auto;
+  /* 去掉之前绝对定位相关的属性 */
+  box-shadow: none;
+  background-color: transparent;
+}
+
+.github-link {
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  align-items: center;
+  color: #409eff;
+  text-decoration: none;
+  transition: opacity 0.2s;
+}
+
+.github-link:hover {
+  opacity: 0.8;
 }
 
 .page-header {
